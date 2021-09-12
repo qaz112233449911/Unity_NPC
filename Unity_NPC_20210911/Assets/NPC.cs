3ac93f27e59cb7425fe3d1235a395e5070e3f388
qaz11223344991199 <qaz112233449911@gmail.com>
2021-09-12 23:31:20 +0800

    NPC方法定義

---
+++ Unity_NPC_20210911/Assets/NPC.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private void Talk(float Talkcontent)
+    {
+
+    }
+    public bool Openstore()
+    {
+        return true;
+    }
+    public int Purchaseprops(float Itemprice = 100f)
+    {
+        return 0;
+    }
+    public void Gettask(float Tasknumber)
+    {
+
+    }
+    private int Taskupdate(float Obtainquestprops = 1f)
+    {
+        return 0;
+    }
+    private bool TaskFinish(float Tasknumber)
+    {
+        return false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
